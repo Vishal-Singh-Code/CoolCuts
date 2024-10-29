ba9cb7524e88df06868573b6dccb4499c4975a00
Vishal Singh <133119527+Vishal18cse@users.noreply.github.com>
2024-10-29 18:40:54 +0530

    test1

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -11,7 +11,7 @@ const Home = () => {
       <section
         className="bg-cover bg-center h-screen"
         style={{
-          backgroundImage: "url('/static/images/home.jpg')",
+          backgroundImage: "url('/Images/home.jpg')",
           paddingTop: "5rem",
           paddingBottom: "5rem" 
         }}
